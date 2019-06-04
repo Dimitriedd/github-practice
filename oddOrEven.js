f76f34148f883fdca5f01487ae069cf51d5f6b2a
Dimitriedd <dimitricollas@gmail.com>
2019-06-04 15:43:05 -0700

    update oddOrEven.js

--- oddOrEven.js
+++ oddOrEven.js
@@ -1,7 +1,9 @@
 // Update the following function to take an argument and output 'Even' if the argument is an even number or 'Odd' if the number is an odd number.
 
-function oddOrEven(){
-    console.log('result')
+function oddOrEven (number){
+    if (number%2===0){console.log(number+" is even")}
+    else if (number%2 !==0){console.log(number+" is odd")} else { console.log("undfined");
+}
 }
 
 oddOrEven(4)
